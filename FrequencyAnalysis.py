7557e85006d5b21037a53a8e186240f299ed1488
Carlos Paredes <45949389+CarloGauss33@users.noreply.github.com>
2019-06-30 22:45:07 -0400

    Add files via upload

--- FrequencyAnalysis.py
+++ FrequencyAnalysis.py
@@ -1,9 +1,6 @@
 ### Made by Carlos Paredes for the course IMT1001 HOMEWORK PUC de Chile
 ### Licenced by MIT LICENSE 2019
 import string
-
-import numpy as np  
-import matplotlib.pyplot as plt
 ################ this libraries will be used for data visualization ##############
 global lowerletters
 lowerletters = 'abcdefghijklmnopqrstuvwxyz'
@@ -17,37 +14,33 @@ non_natural_caracters = []
 for letter in ascii_chars:
     if letter not in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ":
         non_natural_caracters.append(letter)
-print(non_natural_caracters)
 ############################################################################################
 def non_natural_remover(line):
     non_natural = "".join(non_natural_caracters)
-    newline = ""
+    newline = ""    
     for word in line:
         if word not in non_natural:
             newline+=word
     return newline
 #########################Creating the frequency analizer#####################################
-def frecuency_analisis(lines):
-    Distribution_Matrix = []
+def frecuency_analisis(text):
+    words = [k for k in lowerletters]
     globalcount = 0
-    line_to_analize = line.lower()
-    words = []
-    counters = []
-    for word in lowerletters:
-        wordcount = lowerletters.count(word)
-        words.append(word)
-        counters.append(wordcount)
-        globalcount+=wordcount
-    Distribution_Matrix.append(words).append(counters)
+    counters = [0 for i in range(len(words))]
+    for line in text:
+        line.replace(" ","").replace("\n","")
+        trated_line = non_natural_remover(line)
+        frecuency_analisis
+        Distribution_Matrix = []
+        line_to_analize = line.lower()
+        for k in range(len(lowerletters)):
+                wordcount = line_to_analize.count(lowerletters[k])
+                counters[k]=counters[k]+wordcount
+                globalcount+=wordcount
+    Distribution_Matrix.append(words)
+    Distribution_Matrix.append(counters)
     return Distribution_Matrix, globalcount
 #############################################################################################
-text_to_analize = open('non_encripted_file_dir.txt','r')
-lines_to_analize = text_to_analize.readlines()
-text_to_analize.close()
-############################################################################################
-total_letters = 0
-for line in lines_to_analize:
-    line.replace(" ","").replace("\n","")
-    trated_line = non_natural_remover(line)
-    frecuency_analisis
+
+
 
---
+++ FrequencyAnalysisVisualization.py
@@ -0,0 +1,31 @@
+### Made by Carlos Paredes for the course IMT1001 HOMEWORK PUC de Chile
+### Licenced by MIT LICENSE 2019
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import FrequencyAnalysis as freqA
+
+#this script uses numpy and matplotlib to visualizate the data
+
+
+
+text_to_analize = open('non_encriptedfile_dir.txt','r')
+lines_to_analize = text_to_analize.readlines()
+text_to_analize.close()
+#############################################################################################
+data,total = freqA.frecuency_analisis(lines_to_analize)
+
+Y=[(data[1][i]/total)*100 for i in range(len(data[1]))]
+Classes = data[0]
+X = np.arange(len(Classes))
+print(Y)
+print(X)
+
+
+plt.bar(X,Y,align='center',alpha=0.5)
+plt.xticks(X, Classes)
+plt.ylabel('Words (%)')
+plt.title('Frequency')
+
+plt.show()
